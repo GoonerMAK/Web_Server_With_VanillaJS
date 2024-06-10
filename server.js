eb37f1e7efc0e555d04acf9bafc048b0617bfa68
GoonerMAK <mashrurahsan@iut-dhaka.edu>
2024-06-10 13:43:00 +0600

    localhost server now serves all the html pages
    - Index now has navigations

--- server.js
+++ server.js
@@ -3,10 +3,22 @@ const fs = require('fs');
 const port = 4000;
 
 const server = http.createServer((req, res) => {
-    
+
+    let filePath = '';
     res.writeHead(200, { "Content-Type": "text/html" });
 
-    fs.readFile('index.html', (err, data) => {
+    if (req.url === '/') {
+        filePath = 'index.html';
+    } else if (req.url === '/euro') {
+        filePath = 'euro24.html';
+    } else if (req.url === '/wc-t20') {
+        filePath = 'world-cup-t20.html';
+    } else {
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end("404 Page Not Found");
+    }
+
+    fs.readFile(filePath, (err, data) => {
         res.write(data);
         res.end();
     });
